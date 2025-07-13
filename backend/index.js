688ba3962f47619304234ff6021709c2677f620c
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-07-14 00:40:00 +0530

    database-working

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,41 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
-require('dotenv').config();
 const app = express();
-
-
+const user = require("./models/user")
+const todo = require("./models/todo")
+const PORT = 3000;
+require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI)
-.then(()=>{
+.then(async()=>{
     console.log("Connect to MongoDB")
+
+    const testUser = await user.create({
+        username: "testUser",
+        password: "test123"
+});
+
+     await todo.create({
+        title: "First Todo",
+        description: "testing",
+        completed: false,
+        userId: testUser._id
+    })
+    
+console.log("working buddy!")
 })
 .catch((err)=>{
     console.log("Connectione error: ",err)
 })
 
-//routes
-const authRoutes=require("./routes/authRoutes");
-const todoRoutes=require("./routes/todoRoutes");
+
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("api/todos", todoRoutes);
+
+
+
 
 app.listen(3000,()=>{
     console.log(`Server is running on ${PORT}`)
